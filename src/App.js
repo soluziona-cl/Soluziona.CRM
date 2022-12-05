a477da326b714ca195594f0a6500a64650eda441
SunlightShield <sespinozahenriquez@gmail.com>
2022-12-05 11:52:36 -0300

    añadido un componente para subir archivos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from "../src/html/Login";
 import Dashboard from "./html/Dashboard";
+import ImportarArchivo from "../src/html/subirArchivos"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Login />} />
           <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/ImportarArchivo" element={<ImportarArchivo />} />
         </Routes>
     </Router>  
   );
