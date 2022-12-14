4d526e5275e6b0a28c1badf62afdfdb7ab75183f
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2022-12-14 10:00:11 -0300

    arreglos de reportes y exportar

--- src/html/Componentes/ReporteCalidadTabla.js
+++ src/html/Componentes/ReporteCalidadTabla.js
@@ -100,14 +100,14 @@ function ReporteCalidadTabla({ flujo , campana , ini , fin }) {
 
     return (
         <>
-            <section className="flex flex-row ">
+            <section className="float-end">
                 <button
                     onClick={handleOnExportAgente}
                     className="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-                    <i className="fa-solid fa-file-excel mr-2"></i>Exportar
+                    <i className="fa-solid fa-file-excel mr-2"></i> Exportar
                 </button>
             </section>
-            <div className="container mt-2">
+            <div className="mt-5">
 
                 <table className="table">
                     <thead>
